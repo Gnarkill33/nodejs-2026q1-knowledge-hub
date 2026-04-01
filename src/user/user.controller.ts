824b32d1a6fbab92d4b1987e302b096c278786a1
Gnarkill33 <gnarkill33@gmail.com>
2026-04-01 20:36:22 +0300

    feat: add user entity

---
+++ src/user/user.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  HttpCode,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    return this.userService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get('/:id')
+  findOne(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Put('/:id')
+  update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() dto: UpdatePasswordDto,
+  ) {
+    return this.userService.update(id, dto);
+  }
+
+  @Delete('/:id')
+  @HttpCode(204)
+  remove(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.userService.remove(id);
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,74 @@
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import { db, uuid } from 'db';
+import { UserRole } from 'src/types';
+
+@Injectable()
+export class UserService {
+  create(dto: CreateUserDto) {
+    const newUser = {
+      id: uuid(),
+      login: dto.login,
+      password: dto.password,
+      role: dto.role || UserRole.VIEWER,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+
+    const newUserNoPassword = { ...newUser };
+    delete newUserNoPassword.password;
+
+    db.users.push(newUser);
+
+    return newUserNoPassword;
+  }
+
+  findAll() {
+    return db.users;
+  }
+
+  findOne(id: string) {
+    const user = db.users.find((user) => user.id === id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  update(id: string, dto: UpdatePasswordDto) {
+    const existingUser = this.findOne(id);
+
+    if (!existingUser) throw new NotFoundException('User not found');
+
+    if (existingUser.password !== dto.oldPassword)
+      throw new ForbiddenException('Wrong password');
+
+    const updatedUser = {
+      ...existingUser,
+      password: dto.newPassword,
+      updatedAt: Date.now(),
+    };
+
+    db.users = db.users.map((user) => (user.id === id ? updatedUser : user));
+
+    const updatedUserNoPassword = { ...updatedUser };
+    delete updatedUserNoPassword.password;
+
+    return updatedUserNoPassword;
+  }
+
+  remove(id: string) {
+    const existingUser = this.findOne(id);
+
+    if (!existingUser) throw new NotFoundException('User not found');
+
+    db.users = db.users.filter((user) => user.id !== id);
+  }
+}
