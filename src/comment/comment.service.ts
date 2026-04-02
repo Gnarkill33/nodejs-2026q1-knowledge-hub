2e17029730a2d458b54f301901f5a15e4e1b7746
Gnarkill33 <gnarkill33@gmail.com>
2026-04-02 22:10:49 +0300

    feat: add comment entity

---
+++ src/comment/comment.service.ts
@@ -0,0 +1,43 @@
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { db, uuid } from 'db';
+
+@Injectable()
+export class CommentService {
+  create(dto: CreateCommentDto) {
+    const existingArticle = db.articles.find(
+      (article) => article.id === dto.articleId,
+    );
+
+    if (!existingArticle)
+      throw new UnprocessableEntityException(
+        "Article with this ID doesn't exist",
+      );
+
+    const newComment = {
+      id: uuid(),
+      ...dto,
+      createdAt: Date.now(),
+    };
+
+    db.comments.push(newComment);
+
+    return newComment;
+  }
+
+  findAll(articleId: string) {
+    return db.comments.filter((comment) => comment.articleId === articleId);
+  }
+
+  remove(id: string) {
+    const existingComment = db.comments.find((comment) => comment.id === id);
+
+    if (!existingComment) throw new NotFoundException('Comment not found');
+
+    db.comments = db.comments.filter((comment) => comment.id !== id);
+  }
+}
