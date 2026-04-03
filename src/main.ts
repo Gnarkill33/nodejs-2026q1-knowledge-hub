83a4a9f8740cd48402fe3d8cced2800f344d91ee
Gnarkill33 <gnarkill33@gmail.com>
2026-04-04 00:06:26 +0300

    feat: generate swagger at /doc

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './constants';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import * as yaml from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const filePath = join(process.cwd(), 'doc', 'api.yaml');
+  const yamlFile = readFileSync(filePath, 'utf8');
+  const swaggerDocument = yaml.load(yamlFile);
+
+  SwaggerModule.setup('doc', app, swaggerDocument);
 
   await app.listen(PORT);
 }
