821c3b0539df478b313cd3d2c0a0dc2ed7ed119d
Gnarkill33 <gnarkill33@gmail.com>
2026-04-26 20:24:09 +0300

    feat: add role guard for category entity

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -9,30 +9,40 @@ import {
   Put,
   HttpStatus,
   HttpCode,
+  UseGuards,
 } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { RoleGuard } from 'src/role/role.guard';
+import { Roles } from 'src/role/role.decorator';
+import { UserRole } from 'src/types';
 
 @Controller('category')
+@UseGuards(AuthGuard, RoleGuard)
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
+  @Roles(UserRole.ADMIN)
   create(@Body() dto: CreateCategoryDto) {
     return this.categoryService.create(dto);
   }
 
   @Get()
+  @Roles(UserRole.ADMIN, UserRole.VIEWER, UserRole.EDITOR)
   findAll() {
     return this.categoryService.findAll();
   }
 
   @Get('/:id')
+  @Roles(UserRole.ADMIN, UserRole.VIEWER, UserRole.EDITOR)
   findOne(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.categoryService.findOne(id);
   }
 
   @Put('/:id')
+  @Roles(UserRole.ADMIN)
   update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() dto: CreateCategoryDto,
@@ -41,6 +51,7 @@ export class CategoryController {
   }
 
   @Delete('/:id')
+  @Roles(UserRole.ADMIN)
   @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.categoryService.remove(id);
